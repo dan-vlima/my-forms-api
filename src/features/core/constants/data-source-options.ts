fb57a95a3b67f0a8b29d16cd86311e9ab96dff46
Dan Lima <danlima.tech@gmail.com>
2023-06-02 08:17:35 -0300

    refactor: dataSource

--- src/features/core/constants/data-source-options.ts
+++ src/features/core/constants/data-source-options.ts
@@ -2,9 +2,9 @@ import { DataSourceOptions } from 'typeorm';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: '34.151.216.54',
   username: 'postgres',
-  password: 'admin',
+  password: 'jI4|jHIY}$k7Mx_K',
   database: 'my-forms',
   entities: ['dist/**/*-model.js'],
   migrations: ['dist/migrations/*.js'],
